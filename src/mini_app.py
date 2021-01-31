62c87e3c5150a92d05214409fb4bbcc35794bd80
Vivian <viviansokunbi@gmail.com>
2021-01-31 22:49:12 +0000

    update mini app

--- src/mini_app.py
+++ src/mini_app.py
@@ -1,12 +1,34 @@
-products = ["Apple", "Samsung", "Huawei", "Nokia"]
-courier = ["DHL", "Hermes", "Yodel", "Royal Mail"]
-options = ("[0] To Exit app", "[1] For Product Menu", "[2] For Courier Menu")
-product_menu = ("[0] Return to Main Menu ", "[1] View Products ", "[2] Create new product") 
-courier_menu = ("[0] Return to Main Menu ", "[1] View Couriers ", "[2] Create new courier") 
-    
-import os
+main_menu = """
+[0] To Exit app 
+[1] For Product Menu 
+[2] For Courier Menu 
+[3] For Order Menu
+"""
 
-#Products list
+products = ["Apple", "Samsung", "Huawei", "Nokia"]
+product_menu = """
+[0] Return to Main Menu 
+[1] View Products  
+[2] Create new product
+"""
+
+couriers = ["DHL", "Hermes", "Royal Mail"]
+courier_menu = """
+[0] Return to Main Menu 
+[1] View Couriers 
+[2] Create new courier
+"""
+order_menu = """
+[0] Return to Main Menu 
+[1] View Orders 
+[2] Create new order
+[3] Update order status
+"""
+
+import os
+import functions
+
+# #Products list
 with open('products.txt','r+') as f:    
         print(f'Product List: \n')
         for line in f: 
@@ -22,64 +44,57 @@ with open('couriers.txt','r+') as f:
 
 def start_app():
 
-    print("User options:")
-    for i in range(len(options)):
-        print(options[i])
-        
+    print(f"User options: {main_menu}")
+    
     while True :
-        user_input = int(input("Enter option : "))
+        user_input = int(input("Enter option here: "))
         os.system("clear")
         
         if user_input == 0:
-            print("exited app")
+            print("Exited app")
             exit()
             
         elif user_input == 1:
             while True:
-                for i in range(len(product_menu)):
-                    print(product_menu[i])
-                choice = int(input("Enter option: "))
-                
+                print(product_menu)
+                option_1 = int(input("Enter option: "))
                 os.system("clear")
-                
-                if choice == 0:
+                if option_1 == 0:
                     start_app()
-                if choice == 1:
+                if option_1 == 1:
                     print(products)
                     input("press enter to continue")
                     os.system("clear")
-                if choice == 2:
-                    new_product = input("New product name : ")
-                    products.append(new_product)
-                    print(products)
-                    with open("products.txt",'a') as f:
-                        f.write(new_product)
-                        f.write("\n")
-                    input("press enter to continue")
-                    os.system("clear")
+                if option_1 == 2:
+                    functions.new_product()
 
         elif user_input == 2:
             while True:
-                for i in range(len(courier_menu)):
-                    print(courier_menu[i])
-                selected = int(input("Enter choice here"))
+                print(courier_menu)
+                option_2 = int(input("Enter option here: "))
                 os.system("clear")
-                if selected == 0:
+                if option_2 == 0:
                     start_app()
-                if selected == 1:
-                    print(courier)
+                if option_2 == 1:
+                    print(couriers)
                     input("press enter to continue")
                     os.system("clear")
-                if selected == 2:
-                    new_courier = input("New courier name : ")
-                    courier.append(new_courier)
-                    print(courier)
-                    with open("couriers.txt",'a') as f:
-                        f.write(new_courier)
-                        f.write("\n")
-                    input("press enter to continue")
-                    os.system("clear")
+                if option_2 == 2:
+                    functions.new_courier()
+
+        elif user_input == 3:
+            while True:
+                print(order_menu)
+                option_3 = int(input("Enter option here: "))
+                os.system("clear")
+                if option_3 == 0:
+                    start_app()
+                if option_3 == 1:
+                    functions.view_orders()
+                if option_3 == 2:
+                    functions.new_order()
+                if option_3 == 3:
+                    functions.update_order()
         break
 
 start_app()
-
